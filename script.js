5ca7fd823513600781cf3397cf6e033c89c67a62
Diego Ramos <diego_ramos93@outlook.com>
2022-01-27 12:44:37 -0300

    adição de raiz quadrada  e exponenciação

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 
 const names = ["Diego", "Ana", "Paulo", "Maria", "Marcos", "Giovana", "Felipe", "Tereza", "Carlos", "Thais", "Silmar", "Hellen", "Elaine", "Mercedes", "Manoel", "Roberta"];
 
-const numbers = [1, 5, 14, 2, 253, 52, 8, 7, 17, 72, 148, 256, 512, 768, 925, 3, 11, 21, 27, 23, 54, 42, 38, 40, 50, 444, 517, 768, 41, 53, 83, 92, 81, 111, 25, 19, 22];
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
+
+const despensa = [
+    {fruta: 'manga', fruta: 'abacate', fruta:'laranja', fruta:'uva'},
+    {cereal: 'feijão', cereal: 'arroz', cereal: 'soja'},
+    {doce: 'chocolate', doce: 'pé-de-moleque', doce: 'brigadeiro', doce:'doce-de-leite'}
+]
 
 document.write('Arrays: <br/><br/>');
 
 document.write('names: ' + names + '<br /><br /><br />');
 
 document.write('numbers: ' + numbers + '<br /><br /><br /> <hr/>');
+
 
 // Array numbers 
 
@@ -30,6 +37,19 @@ document.write('The Smallest number is = ' + smallNumber + '<br /><br />');
 
 let sum = (accumulator, curr) => accumulator + curr;
 document.write('The sum of array numbers is: ' + numbers.reduce(sum) + '<br/><br/> <hr/>');
+
+// Square Root
+
+let square = numbers.map(function (num) {
+    return Math.sqrt(num)
+});
+
+// potentiation
+
+let pot = numbers.map(function (num) {
+return Math.pow(num, 7);
+
+})
 
 //Array names
 
